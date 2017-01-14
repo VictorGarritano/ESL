b32fc90502512f881bbbe6be002d6badb1f875ce
VictorGarritano <victorg@ufrj.br>
2017-01-14 13:58:59 -0200

    PCA
    Análise dos componentes principais do "diabetes dataset" do sklearn

---
+++ pca.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import load_diabetes
+import numpy as np 
+from numpy.linalg import svd
+from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt 
+
+X, y = load_diabetes(return_X_y=True)
+X_r = scale(X, with_std=False, with_mean=True) #mean = 0
+(N, p) = X_r.shape
+print N 
+print p 
+U, d, V = svd(X_r, full_matrices=False)
+D = np.diag(d)
+print U.shape 	#N x p orthogonal matrix, whose columns
+				#are called left singular vectors
+
+print V.shape	#p x p orthogonal matrix, whose columns
+				#are called right singular vectors
+
+print D.shape	#p x p diagonal matrix, with diagonal
+				#elements known as singular values
+
+PC = np.dot(U, D) #Principal components of X_r
+
+plt.scatter(PC[:, 0], PC[:, 1]) #The first two principal
+								#components of the data
+plt.xlabel('First Principal Component')
+plt.ylabel('Second Principal Component')
+plt.title('The first two principal components of the data')
+plt.show()
